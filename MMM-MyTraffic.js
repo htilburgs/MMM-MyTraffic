aeb18af469b1df46680c04c76c5153151bdcffd7
Harm Tilburgs <24824989+htilburgs@users.noreply.github.com>
2019-04-16 15:03:23 +0200

    Update MMM-MyTraffic.js

--- MMM-MyTraffic.js
+++ MMM-MyTraffic.js
@@ -93,7 +93,7 @@ Module.register('MMM-MyTraffic', {
 			event.className = "description xsmall";
 			var information = document.createElement("div");
 			information.className = "information bold"
-			if (jams[i].jam.startDate !== "undefined") {
+			if (typeof jams[i].jam.startDate !== "undefined") {
 				information.innerHTML = jams[i].name + " - " + jams[i].jam.startDate + " - " + (jams[i].jam.distance/1000) + "KM";
 				} else {
 				information.innerHTML = jams[i].name;
